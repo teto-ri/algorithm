fe0c2b5cd331e4e8f01dbf34c8de4bc4353a264a
ByeongGeun Shin <byeonggeun.ml@gmail.com>
2024-01-09 13:34:03 +0900

    [Bronze I] Title: 평균, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,8 +1,3 @@
-def avg(num):
-    return sum(num) / len(num)
-
 N = int(input())
 num = list(map(int, input().split()))
-maximum = max(num)
-num = [i / maximum * 100 for i in num]
-print(avg(num))
+print(sum(num)/max(num)*100/N)
